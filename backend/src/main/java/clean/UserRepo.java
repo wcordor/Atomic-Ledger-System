28b630fda8046509c942dc61ebd5bca80ded096c
William Cordor <william@Rig.localdomain>
2026-07-01 18:34:32 -0400

    refactor: remove unused locking queries from UserRepo

--- backend/src/main/java/clean/UserRepo.java
+++ backend/src/main/java/clean/UserRepo.java
@@ -6,11 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepo extends CrudRepository<User, Long> {
 
-    /*@Lock(LockModeType.PESSIMISTIC_WRITE)
-    @QueryHints({@QueryHint(name = "jakarta.persistence.lock.timeout", value = "10000")})
-    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
-    Optional<User> findWithLockingById(Long id);*/
-
     List<User> findByLastName(String lastName);
     User findById(long id);
 }

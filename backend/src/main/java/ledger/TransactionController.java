6578f9ba0fe0a574c82d9fb55cf819867b8df983
wcordor <cordorwilliam@gmail.com>
2026-07-06 13:06:15 -0400

    'cancel' and 'complete' functions added to transaction controller

--- backend/src/main/java/ledger/TransactionController.java
+++ backend/src/main/java/ledger/TransactionController.java
@@ -1,14 +1,21 @@
 package ledger;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -53,4 +60,33 @@ public class TransactionController {
                 .body(assembler.toModel(newTransaction));
     }
    
+    @DeleteMapping("/transactions/{id}/cancel")
+    public ResponseEntity<?> cancel(@PathVariable Long id) {
+
+        Transaction transaction = transactionRepo.findById(id)
+            .orElseThrow(() -> new TransactionNotFoundException(id));
+
+        if (transaction.getStatus() == Status.PENDING) {
+            transaction.setStatus(Status.CANCELED);
+            return ResponseEntity.ok(assembler.toModel(transactionRepo.save(transaction)));
+        }
+
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE)
+                .body("You can't cancel a transaction that is in the " + transaction.getStatus() + " status");
+    }
+
+    @PutMapping("/transactions/{id}/complete")
+    public ResponseEntity<?> complete(@PathVariable Long id) {
+
+        Transaction transaction = transactionRepo.findById(id)
+            .orElseThrow(() -> new TransactionNotFoundException(id));
+
+        if (transaction.getStatus() == Status.PENDING) {
+            transaction.setStatus(Status.SUCCESSFUL);
+            return ResponseEntity.ok(assembler.toModel(transactionRepo.save(transaction)));
+        }
+
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE)
+                .body("You can't complete a transaction that is in the " + transaction.getStatus() + " status");
+    }
 }

97e72432886529ed2e548374f96def4b6b6e13da
wcordor <cordorwilliam@gmail.com>
2026-07-16 16:53:49 -0400

    style: @EntityGraph line properly indented

--- backend/src/main/java/ledger/AccountRepo.java
+++ backend/src/main/java/ledger/AccountRepo.java
@@ -20,7 +20,7 @@ public interface AccountRepo extends JpaRepository<Account, Long> {
     Account findById(long id);
     List<Account> findByUserLastName(String lastName);
     List<Account> findByCurrency(String currency);
-   @EntityGraph(attributePaths = {"sent", "received"})
+    @EntityGraph(attributePaths = {"sent", "received"})
     @Query("SELECT a FROM Account a WHERE a.id = :id")
     Optional<Account> findWithTransactions(@Param("id") Long id);
 }

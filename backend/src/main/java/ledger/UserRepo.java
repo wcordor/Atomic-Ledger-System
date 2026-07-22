b05eb16d59642672dfc2b1f98e35165f8f0c6fcc
wcordor <cordorwilliam@gmail.com>
2026-07-22 19:57:23 -0400

    fix: findById() will load user with account list

--- backend/src/main/java/ledger/UserRepo.java
+++ backend/src/main/java/ledger/UserRepo.java
@@ -1,12 +1,18 @@
 package ledger;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepo extends JpaRepository<User, Long> {
 
     List<User> findByLastName(String lastName);
-    User findById(long id);
+    @EntityGraph(attributePaths = {"accounts"})
+    @Query("SELECT u FROM User u WHERE u.id = :id")
+    Optional<User> findById(@Param("id") Long id);
 }
      

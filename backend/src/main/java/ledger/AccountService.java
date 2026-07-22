ae54df8fa3e8c5ef400200b670a7153a31ea6d71
wcordor <cordorwilliam@gmail.com>
2026-07-22 19:31:29 -0400

    Account service class added

---
+++ backend/src/main/java/ledger/AccountService.java
@@ -0,0 +1,30 @@
+package ledger;
+
+import java.math.BigDecimal;
+
+import org.springframework.stereotype.Service;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class AccountService {
+
+    private final AccountRepo accountRepo;
+    private final UserRepo userRepo;
+
+    public AccountService(AccountRepo accountRepo, UserRepo userRepo) {
+        this.accountRepo = accountRepo;
+        this.userRepo = userRepo;
+    }
+
+    public Account createAccount(/*User user*/Long userId, String name, BigDecimal initialDeposit, String currency) {
+        User user = userRepo.findById(userId).orElseThrow(() -> new EntityNotFoundException("User not found"));
+        Account account = new Account(user, name, initialDeposit, currency);
+        accountRepo.save(account);
+        account = accountRepo.findWithTransactions(account.getId())
+        .orElseThrow(() -> new EntityNotFoundException("Account not found"));
+        
+        return account;
+    }
+
+}

a7bccc67c6a4afd3129168ae45750a703d189a40
wcordor <cordorwilliam@gmail.com>
2026-07-31 12:30:09 -0400

    style: user RestControllerAdvice class properly renamed

--- backend/src/main/java/com/github/wcordor/ledger/UserNotFoundAdvice.java
+++ backend/src/main/java/com/github/wcordor/ledger/UserNotFoundAdvice.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class UserControllerAdvice {
+public class UserNotFoundAdvice {
     
     @ExceptionHandler(UserNotFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)

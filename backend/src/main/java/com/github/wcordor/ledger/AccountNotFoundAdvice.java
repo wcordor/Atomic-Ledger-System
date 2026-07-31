8e93d130434cbca11f89f25956f6dc96440e9029
wcordor <cordorwilliam@gmail.com>
2026-07-31 12:29:25 -0400

    feat: AccountNotFound exception and advice classes created

---
+++ backend/src/main/java/com/github/wcordor/ledger/AccountNotFoundAdvice.java
@@ -0,0 +1,17 @@
+package com.github.wcordor.ledger;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class AccountNotFoundAdvice {
+
+    @ExceptionHandler(AccountNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String accountNotFoundHandler(AccountNotFoundException e) {
+		return e.getMessage();
+	}
+    
+}
---
+++ backend/src/main/java/com/github/wcordor/ledger/AccountNotFoundException.java
@@ -0,0 +1,9 @@
+package com.github.wcordor.ledger;
+
+public class AccountNotFoundException extends RuntimeException {
+
+    public AccountNotFoundException(Long accountId, Long userId) {
+		super("Account " + accountId + " either does not exist, or does not belong to User " + userId + ".");
+	}
+
+}

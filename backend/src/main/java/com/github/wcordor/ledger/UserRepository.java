7cc0541d1c5bb2b2f46b36e2689b2516dd68dfda
wcordor <cordorwilliam@gmail.com>
2026-07-31 12:24:13 -0400

    feat: user repository given pessimistic locking method

--- backend/src/main/java/com/github/wcordor/ledger/UserRepository.java
+++ backend/src/main/java/com/github/wcordor/ledger/UserRepository.java
@@ -5,14 +5,22 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import jakarta.persistence.LockModeType;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     List<User> findByLastName(String lastName);
+
     @EntityGraph(attributePaths = {"accounts"})
     @Query("SELECT u FROM User u WHERE u.id = :id")
     Optional<User> findById(@Param("id") Long id);
+    
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT u FROM User u WHERE u.id = :id")
+    Optional<User> findWithLockingById(Long id);
 }
      

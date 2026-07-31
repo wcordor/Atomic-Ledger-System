e7407d113b892dd750cc2f479f2cd473168cd1b5
wcordor <cordorwilliam@gmail.com>
2026-07-31 17:04:05 -0400

    feat: locking method for findByIdAndUser_Id() added

--- backend/src/main/java/com/github/wcordor/ledger/AccountRepository.java
+++ backend/src/main/java/com/github/wcordor/ledger/AccountRepository.java
@@ -28,4 +28,7 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     
     List<Account> findByUser_Id(Long id);
     Optional<Account> findByIdAndUser_Id(Long accountId, Long userId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<Account> findWithLockingByIdAndUser_Id(Long accountId, Long userId);
 }

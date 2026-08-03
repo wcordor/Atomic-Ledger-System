aeece0483637a0e1506511966c7c31a695e979e2
wcordor <cordorwilliam@gmail.com>
2026-08-03 16:36:29 -0400

    refactor: transaction repository renamed

--- backend/src/main/java/com/github/wcordor/ledger/TransactionRepository.java
+++ backend/src/main/java/com/github/wcordor/ledger/TransactionRepository.java
@@ -2,7 +2,6 @@ package com.github.wcordor.ledger;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransactionRepo extends JpaRepository<Transaction, Long> {
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
-    
 }

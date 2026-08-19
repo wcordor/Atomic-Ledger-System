e0e73993ab7b2186f120a5d39b41873147bd3fba
wcordor <cordorwilliam@gmail.com>
2026-08-19 08:05:37 -0400

    style: user account list declared before constructor

--- backend/src/main/java/com/github/wcordor/ledger/entity/User.java
+++ backend/src/main/java/com/github/wcordor/ledger/entity/User.java
@@ -16,15 +16,15 @@ public class User {
     private String firstName;
     private String lastName;
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.MERGE, orphanRemoval = true)
+    private List<Account> accounts = new ArrayList<>();
+
     protected User() {}
 
     public User(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.MERGE, orphanRemoval = true)
-    private List<Account> accounts = new ArrayList<>();
 
     @Override
     public String toString() {

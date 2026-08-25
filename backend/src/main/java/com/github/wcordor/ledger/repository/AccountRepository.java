84b10b2626f66d37f16f56fea85612d523fe903a
wcordor <cordorwilliam@gmail.com>
2026-08-25 11:37:27 -0400

    feat: repositories given @Repository annotation

--- backend/src/main/java/com/github/wcordor/ledger/repository/AccountRepository.java
+++ backend/src/main/java/com/github/wcordor/ledger/repository/AccountRepository.java
@@ -8,11 +8,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.github.wcordor.ledger.entity.Account;
 
 import jakarta.persistence.LockModeType;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
     
     @Lock(LockModeType.PESSIMISTIC_WRITE)

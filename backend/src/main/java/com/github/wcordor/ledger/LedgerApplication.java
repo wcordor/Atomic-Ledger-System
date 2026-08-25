781ae4236be372213518545dc153ac55b289d397
wcordor <cordorwilliam@gmail.com>
2026-08-25 12:05:02 -0400

    refactor: moneyTransfer() method replaced with demo money transfer method to reduce redundancy

--- backend/src/main/java/com/github/wcordor/ledger/LedgerApplication.java
+++ backend/src/main/java/com/github/wcordor/ledger/LedgerApplication.java
@@ -150,8 +150,7 @@ public class LedgerApplication {
 				logger.info(String.format("Balances before transfer: Account %d - %,.2f %s, Account %d - %,.2f %s",
 					account7_id, account7_bal, account7_currency, account5_id, account5_bal, account5_currency));
 
-				transactionService.moneyTransfer(UUID.randomUUID().toString(), user3_id, account7_id, account5_id,
-					new BigDecimal("2000.00"), "USD");
+				requestFactory.demoMoneyTransfer(user3_id, account7_id, account5_id, new BigDecimal("2000.00"), "USD");
 
 				logger.info("");
 			} catch (InsufficientFundsException e) {
@@ -196,7 +195,7 @@ public class LedgerApplication {
 			for (int i = 0; i < 20; i++) {
 				CompletableFuture<Void> future1 = CompletableFuture.runAsync(() -> {
 					try {
-						transactionService.moneyTransfer(UUID.randomUUID().toString(), user2_id, account5_id, account9_id, new BigDecimal("50.00"), "USD");
+						requestFactory.demoMoneyTransfer(user2_id, account5_id, account9_id, new BigDecimal("50.00"), "USD");
 					} catch (InsufficientFundsException e) {
 						logger.info("");
 						logger.error("ERROR: " + e.getMessage());
@@ -207,7 +206,7 @@ public class LedgerApplication {
 
 				CompletableFuture<Void> future2 = CompletableFuture.runAsync(() -> {
 					try {
-						transactionService.moneyTransfer(UUID.randomUUID().toString(), user3_id, account7_id, account9_id, new BigDecimal("25.00"), "USD");
+						requestFactory.demoMoneyTransfer(user3_id, account7_id, account9_id, new BigDecimal("25.00"), "USD");
 					} catch (InsufficientFundsException e) {
 						logger.info("");
 						logger.error("ERROR: " + e.getMessage());

7ab4ef98850800640eb2cd6218f7bd21bfdb7679
wcordor <cordorwilliam@gmail.com>
2026-09-01 12:42:50 -0400

    feat: InsufficientFundsException and EntityNotFoundException added to TransactionExceptionAdvice

--- backend/src/main/java/com/github/wcordor/ledger/advice/TransactionExceptionAdvice.java
+++ backend/src/main/java/com/github/wcordor/ledger/advice/TransactionExceptionAdvice.java
@@ -5,8 +5,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.github.wcordor.ledger.exception.InsufficientFundsException;
 import com.github.wcordor.ledger.exception.InvalidTransferException;
 import com.github.wcordor.ledger.exception.TransactionNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @RestControllerAdvice
 public class TransactionExceptionAdvice {
@@ -20,6 +23,18 @@ public class TransactionExceptionAdvice {
     @ExceptionHandler(InvalidTransferException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     public String invalidTransferHandler(InvalidTransferException e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(InsufficientFundsException.class)
+    @ResponseStatus(HttpStatus.PAYMENT_REQUIRED)
+    public String insufficientFundsHandler(InsufficientFundsException e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String entityNotFoundHandler(EntityNotFoundException e) {
         return e.getMessage();
     }
 }

ffba8fc0861989a683a7078e3bcf90af4243c1e9
wcordor <cordorwilliam@gmail.com>
2026-09-01 13:22:34 -0400

    chore: spring boot test dependency updated to include WebMvc tests

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -19,7 +19,7 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
